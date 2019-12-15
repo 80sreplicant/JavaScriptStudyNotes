f3ac14dbc1a587ff6da6fea3e352441ba41d1e3d
80sreplicant <jdnyc84@gmail.com>
2019-12-15 13:32:43 -0500

    Added some more files and changed things

--- creatingWithStrings2.js
+++ creatingWithStrings2.js
@@ -5,19 +5,17 @@
 // const itemImage = document.querySelector(`.wrapper img`);
 
 // itemImage.classList.add(`round`);
-// 
+//
 
 const item = document.querySelector(`.item`);
 
 const src = `https://picsum.photos/400`;
 const alt = `Baby Yoda Here`;
 
-const myHTML = 
-`<div class="wrapper">
+const myHTML = `<div class="wrapper">
 <h1>${alt}</h1>
 <img src="${src}" alt="${alt}">
 </div>`;
-
 
 // Turn a string into a DOM element
 const myFragment = document.createRange().createContextualFragment(myHTML);
